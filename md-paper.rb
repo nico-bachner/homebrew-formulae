a9fd37ed8d761e52f97cdefa44b752aa75bf8664
Nico Bachner <yo@nicob.dev>
2020-11-24 10:00:43 +0100

    fix sha mismatch

--- md-paper.rb
+++ md-paper.rb
@@ -2,7 +2,7 @@ class MdPaper < Formula
   desc "Generates scientific papers written in Markdown"
   homepage "https://md-paper.now.sh"
   url "https://github.com/nico-bachner/md-paper/archive/v0.1.3.tar.gz"
-  sha256 "89c65ea8dd1f03b8574933a3a8dd0653b0eb3df03e365bdcd338e362d1536242"
+  sha256 "fd5f9db3196efc40fe0cbe2451c934fb07d5e1d42c4ff6d40babfddc17c6815f"
   license "MIT"
   
   depends_on "pandoc"
