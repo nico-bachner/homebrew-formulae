9760bbf7de5a9c822122e12608db617e020199d3
Nico Bachner <yo@nicob.dev>
2020-11-24 09:57:21 +0100

    bump to v0.1.3

--- md-paper.rb
+++ md-paper.rb
@@ -1,7 +1,7 @@
 class MdPaper < Formula
   desc "Generates scientific papers written in Markdown"
   homepage "https://md-paper.now.sh"
-  url "https://github.com/nico-bachner/md-paper/archive/v0.1.2.tar.gz"
+  url "https://github.com/nico-bachner/md-paper/archive/v0.1.3.tar.gz"
   sha256 "89c65ea8dd1f03b8574933a3a8dd0653b0eb3df03e365bdcd338e362d1536242"
   license "MIT"
   
